1f3c31dceef2445041c3cde0127523bced32cd3d
stonega <xijieyin@gmail.com>
2024-09-01 14:25:57 +0800

    fix: variable name

--- lua/vue-goto-definition/autocmd.lua
+++ lua/vue-goto-definition/autocmd.lua
@@ -11,14 +11,14 @@ function M.override_definition()
 	local group = vim.api.nvim_create_augroup("VueGotoDefinition", { clear = true })
 	vim.api.nvim_clear_autocmds({ group = group })
 	local opts = config.get_opts()
-  Log.info("Overriding vim.lsp.buf.definition")
+	Log.info("Overriding vim.lsp.buf.definition")
 	vim.api.nvim_create_autocmd({ "FileType" }, {
 		pattern = opts.filetypes,
 		group = group,
 		callback = function()
-			local goto = lsp.get_goto()
+			local go_to = lsp.get_goto()
 			---@diagnostic disable-next-line: duplicate-set-field
-			vim.lsp.buf.definition = goto
+			vim.lsp.buf.definition = go_to
 		end,
 	})
 end
